04435a82974d0ad807e2cbb7eaeed170a0ce997d
gdmosher <gdmosher@calgro.com>
2016-05-19 00:16:18 -0700

    Done, works, tested.
    May be too sloppy.

--- MerkleTree/main.cpp
+++ MerkleTree/main.cpp
@@ -41,16 +41,19 @@ int main(int argc, char** argv) {
     int randNum=rand();
     const int HXSIZE=41;
     char hex[HXSIZE];;
-    cout<<"The initial random number = "<<randNum<<endl;
+	unsigned char code[20];
+/*
+	cout << "The initial random number = " << randNum << endl;
     //Create a message
     string msg="What a difference a day makes!";
     cout<<"The base message = "<<msg<<endl;
-    //Setup the number of loops and loop variables
-    int endLoop=5,freq[HXSIZE]={},cntZero=0;
+*/
+	//Setup the number of loops and loop variables
+    int endLoop=3,freq[HXSIZE]={},cntZero=0;
     //Record the start time
     int strTime=time(0);
     //Loop until you have reached the number of zeroes desired
-    /*
+ /*
 	do{
         //Create a concatenation of the two by first converting the random
         //number to a string
@@ -64,13 +67,11 @@ int main(int argc, char** argv) {
         strcpy_s(cMsg, newMsg.c_str());
         int size=sizeof(cMsg)-1;
         //utilize the sha1 function
-        unsigned char code[20];
         calc(cMsg, size, code);
         toHexString(code, hex);
         cntZero=count0(hex,endLoop+1);
         freq[cntZero]++;
     }while(cntZero<endLoop);
-    */
 	//Take the time it took
     int endTime=time(0);
     //Output the result
@@ -78,7 +79,94 @@ int main(int argc, char** argv) {
     cout<<"The number of zeroes = "<<cntZero<<endl;
     cout<<"The hex output = "<<hex<<endl;
     prntRes(freq,cntZero);
-    //Exit stage right
+*/
+	/**************************************************************/
+	char cMsg[100];
+	int size;
+	string L1 = "\
+		Then out spake brave Horatius,\n\
+		The Captain of the Gate : ";
+	string L2 = "\n\
+		To every man upon this earth\n\
+		Death cometh soon or late.";
+	string L3 = "\n\
+		And how can man die better\n\
+		Than facing fearful odds,\n\
+		and";
+	string L4 = "\n\
+		For the ashes of his fathers,\n\
+		And the temples of his Gods.";
+	cout << L1 << L2 << L3 << L4 << endl;
+	//L1
+	//Convert to a character message
+	strcpy_s(cMsg, L1.c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash00 = hex;
+	cout << "hash00\t" << hash00 << endl;
+	//L2
+	//Convert to a character message
+	strcpy_s(cMsg, L2.c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash01 = hex;
+	cout << "hash01\t" << hash01 << endl;
+	//L3
+	//Convert to a character message
+	strcpy_s(cMsg, L3.c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash10 = hex;
+	cout << "hash10\t" << hash10 << endl;
+	//L4
+	//Convert to a character message
+	strcpy_s(cMsg, L4.c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash11 = hex;
+	cout << "hash11\t" << hash11 << endl;
+	//hash0
+	//Convert to a character message
+	strcpy_s(cMsg, (hash00 + hash01).c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash0 = hex;
+	cout << "hash0\t" << hash0 << endl;
+	//hash1
+	//Convert to a character message
+	strcpy_s(cMsg, (hash10 + hash11).c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hash1 = hex;
+	cout << "hash1\t" << hash1 << endl;
+	//hashTop
+	//Convert to a character message
+	strcpy_s(cMsg, (hash0 + hash1).c_str());
+	size = sizeof(cMsg) - 1;
+	//utilize the sha1 function
+	calc(cMsg, size, code);
+	toHexString(code, hex);
+	string hashTop = hex;
+	cout << "hashTop\t" << hashTop << endl;
+	cout << "Results are pasted to .cpp file, Change L1, re-run," << endl;
+	cout << "compare results also pasted to.cpp file." << endl;
+	cout << "hash00, hash0, and hashTop changed as expected." << endl;
+	cout << "Run and paste comparisons for changes in L2, L3, and L4 also." << endl;
+
+	/**************************************************************/
+	//Exit stage right
 
 	system("Pause");
     return 0;
@@ -99,3 +187,111 @@ int count0(char str[],int n){
     return cnt;
 }
 
+/*
+Then out spake brave Horatius,
+The Captain of the Gate :
+To every man upon this earth
+Death cometh soon or late.
+And how can man die better
+Than facing fearful odds,
+and
+For the ashes of his fathers,
+And the temples of his Gods.
+hash00  13b8260ef27b7682f88ae1b2c54d0ea7e6d45927
+hash01  ed07d41fbad8d4e01ec893de449bea5c5589a5b6
+hash10  6dbbaefbb2f989da30b22dd255ade35dd0107a9e
+hash11  23595367095aae85668b47386c4b63a0b4731346
+hash0   5063fd126ab6c91ad9e2800ae214f81bf57b6bf3
+hash1   7d5dc2b98487cb921dd7ce8ac87749b66ee2df18
+hashTop 41a81c9cce800569722eacf694405c6c2097fa85
+Press any key to continue . . .
+
+L1 is changed
+Then out spake brave Horatius,
+The Captain of the Gate :
+To every man upon this earth
+Death cometh soon or late.
+And how can man die better
+Than facing fearful odds,
+and
+For the ashes of his fathers,
+And the temples of his Gods.
+hash00  449931f3cc1a4b848155634a903a817e08f4e78b
+hash01  ed07d41fbad8d4e01ec893de449bea5c5589a5b6
+hash10  6dbbaefbb2f989da30b22dd255ade35dd0107a9e
+hash11  23595367095aae85668b47386c4b63a0b4731346
+hash0   b22c13557c333db5d6eefc830a2a77092983b3c7
+hash1   7d5dc2b98487cb921dd7ce8ac87749b66ee2df18
+hashTop 26fc29c92f49b7f130aa2cf520bfafde1266ecf0
+Results are pasted to .cpp file, Change L1, re-run, compare results also pasted to .cpp file.
+Press any key to continue . . .
+
+L2 is changed
+Then out spake brave Horatius,
+The Captain of the Gate :
+To every man upon this earth
+Death cometh soon or late.
+And how can man die better
+Than facing fearful odds,
+and
+For the ashes of his fathers,
+And the temples of his Gods.
+hash00  13b8260ef27b7682f88ae1b2c54d0ea7e6d45927
+hash01  27902e4fd9f5683e1a0c0daa02835745fd0ce1c4
+hash10  6dbbaefbb2f989da30b22dd255ade35dd0107a9e
+hash11  23595367095aae85668b47386c4b63a0b4731346
+hash0   7cec2cfbe7b550a46444990ca8c3d769cf3aafed
+hash1   7d5dc2b98487cb921dd7ce8ac87749b66ee2df18
+hashTop 73ce980d800744bc8feb71b3d3df9906fe124b23
+Results are pasted to .cpp file, Change L1, re-run,
+compare results also pasted to.cpp file.
+hash00, hash0, and hashTop changed as expected.
+Run and paste comparisons for changes in L2, L3, and L4 also.
+Press any key to continue . . .
+
+L3 is changed
+Then out spake brave Horatius,
+The Captain of the Gate :
+To every man upon this earth
+Death cometh soon or late.
+And how can man die better
+Than facing fearful odds,
+and
+For the ashes of his fathers,
+And the temples of his Gods.
+hash00  13b8260ef27b7682f88ae1b2c54d0ea7e6d45927
+hash01  ed07d41fbad8d4e01ec893de449bea5c5589a5b6
+hash10  26f60dc0a3e5b7b0d9adb3c701a89aed9b908b30
+hash11  23595367095aae85668b47386c4b63a0b4731346
+hash0   5063fd126ab6c91ad9e2800ae214f81bf57b6bf3
+hash1   ceb7309366ba0f53951000bc1a6344b56a3778b3
+hashTop 7537a24453016774c97d5a1410a082d9b65162dc
+Results are pasted to .cpp file, Change L1, re-run,
+compare results also pasted to.cpp file.
+hash00, hash0, and hashTop changed as expected.
+Run and paste comparisons for changes in L2, L3, and L4 also.
+Press any key to continue . . .
+
+L4 is changed
+Then out spake brave Horatius,
+The Captain of the Gate :
+To every man upon this earth
+Death cometh soon or late.
+And how can man die better
+Than facing fearful odds,
+and
+For the ashes of his fathers,
+And the temples of his Gods.
+hash00  13b8260ef27b7682f88ae1b2c54d0ea7e6d45927
+hash01  ed07d41fbad8d4e01ec893de449bea5c5589a5b6
+hash10  6dbbaefbb2f989da30b22dd255ade35dd0107a9e
+hash11  bc26383ea3250747dbd0d89b2b3c0a81989d441e
+hash0   5063fd126ab6c91ad9e2800ae214f81bf57b6bf3
+hash1   946b648d748a0d42772abfb0c42dcfd9da649491
+hashTop a671686d30b298aca68acb55af31d449521d6a63
+Results are pasted to .cpp file, Change L1, re-run,
+compare results also pasted to.cpp file.
+hash00, hash0, and hashTop changed as expected.
+Run and paste comparisons for changes in L2, L3, and L4 also.
+Press any key to continue . . .
+*/
